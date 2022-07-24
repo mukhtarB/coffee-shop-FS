6c5e26d66df2546657d06554e8d8b24ccafed7ab
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-07-24 20:44:46 +0100

    update frontend env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-utfmv3hr.us.auth0', // the auth0 domain prefix
+    audience: 'drinks', // the audience set for the auth0 app
+    clientId: 'DeQk8jFTLpAt1MQvKQhkfArHopQjJjKP', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
