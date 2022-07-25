1820e06a6d27a3319d95a27affac6fe01da33a1d
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-07-25 13:26:34 +0100

    update FE env vars

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-utfmv3hr.us.auth0', // the auth0 domain prefix
+    url: 'dev-utfmv3hr.us', // the auth0 domain prefix
     audience: 'drinks', // the audience set for the auth0 app
     clientId: 'DeQk8jFTLpAt1MQvKQhkfArHopQjJjKP', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
